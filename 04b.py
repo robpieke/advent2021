c1a41d7c97c2f142c8d3891d54d92182788d45a7
Rob Pieké <robp@sidefx.com>
2021-12-14 11:48:45 +0000

    Day 4

---
+++ 04b.py
@@ -0,0 +1,34 @@
+with open('data/04.txt') as f:
+    data = f.read().splitlines()
+
+numbers = [int(x) for x in data[0].split(',')]
+boards = []
+
+for i in range(2, len(data), 6):
+    boards.append([int(x) for d in data[i:i+5] for x in d.split()])
+
+bmasks = [2**26-1] * len(boards)
+
+winners = [0]*10
+for i in range(5):
+    for j in range(5):
+        winners[i] += 2**(i*5+j)
+        winners[i+5] += 2**(i+j*5)
+
+won = {}
+for n in numbers:
+    for bi, b in enumerate(boards):
+        if bi in won:
+            continue
+        for si, s in enumerate(b):
+            if s == n:
+                bmasks[bi] -= 2**si
+                break
+        for w in winners:
+            if bmasks[bi] & w == 0:
+                m = 0
+                for i in range(25):
+                    if bmasks[bi] & 2**i:
+                        m += boards[bi][i]
+                won[bi] = m*n
+                print(bi, m*n)
